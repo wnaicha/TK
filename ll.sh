68234afcaf0ff9e42a3dc23cb68476525ca60477
wnaicha <140796819+wnaicha@users.noreply.github.com>
2026-04-10 12:56:54 +0800

    Update ll.sh

--- ll.sh
+++ ll.sh
@@ -5,7 +5,7 @@ retr_idx=$(awk '/Tcp:/ {for(i=1;i<=NF;i++) if($i=="RetransSegs") print i}' $snmp
 out_segs=$(echo $data | awk "{print \$$out_idx}"); \
 retr_segs=$(echo $data | awk "{print \$$retr_idx}"); \
 \
-# 关键修复：放大10000倍转为整数比较
+
 rate_int=$(( retr_segs * 1000000 / out_segs )); \
 rate_show=$(awk "BEGIN {printf \"%.4f\", $rate_segs/$out_segs*100}"); \
 \
@@ -23,9 +23,7 @@ clear; \
 echo "==============================================="; \
 echo "📊 TikTok 环境网络质量检测"; \
 echo "==============================================="; \
-printf "📤 总 发 包 : %s\n" "$out_segs"; \
-printf "🔄 重 传 包 : %s\n" "$retr_segs"; \
-printf "📉 重 传 率 : \033[1m%s%%\033[0m\n" "$rate_show"; \
+
 printf "🚦 质量等级 : %b\n" "$level"; \
 echo "==============================================="; \
 printf "\n"
